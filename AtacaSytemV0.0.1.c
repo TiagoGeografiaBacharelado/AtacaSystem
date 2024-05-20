7ec903083390840be2f163a0b63451c855e63bde
TiagoCarneiroDoNascimento <107155765+TiagoGeografiaBacharelado@users.noreply.github.com>
2024-05-20 16:03:06 -0300

    Add files via upload
    Esboço de tela de apresentação ao usuário, biblioteca "locale.h" adicionada.

---
+++ AtacaSytemV0.0.1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main(){
+	setlocale(LC_ALL, "Portuguese");
+	
+	printf("");
+	printf("\t\t\t\t#########################################################\n");
+    printf("\t\t\t\t#                                                       #\n");
+	printf("\t\t\t\t#             Ben-vindo ao AtacaSystem                  #\n");
+	printf("\t\t\t\t#                                                       #\n");
+    printf("\t\t\t\t#########################################################\n");
+
+        printf("\n\t\t\t\t                   1- Login                      \n");
+        printf("\t\t\t\t                   2- Desenvolvedores                             \n");
+        printf("\t\t\t\t                   3- Sair                        \n");
+        printf("\t\t\t\t                                                       \n");
+        printf("\t\t\t\t                   Opção: ");
+}
