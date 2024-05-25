26dc944ee7740775be8b4f9d71b80afeb930023c
SarettoAdriel <166669937+SarettoAdriel@users.noreply.github.com>
2024-05-24 23:48:29 -0300

    Update AtacaSystemV0.0.5.c

--- AtacaSystemV0.0.5.c
+++ AtacaSystemV0.0.5.c
@@ -4,7 +4,8 @@
 #include <stdlib.h>
 #include <conio.h>
 #include <stdio.h>
-#include <time.h>
+
+#define MAX 10
 
 #include "AtacaSystemMenu.c"
 
@@ -15,13 +16,14 @@ typedef struct{
 	int price;
 }Product;
 
+int cod = 0;
+
 void menu(), listProducts();
 int addProduct(), searchProduct();
 
 int main(){
 	setlocale(LC_ALL, "Portuguese");
 	
-	printLogo();
     menu();
 }
 
@@ -29,25 +31,27 @@ void menu(){
 	int choice;
 	
 	do{
-        printf("Escolha uma opção: \n");
-        printf("1 - Cadastrar Produto\n");
+        printLogo();
+		
+		printf("\n1 - Cadastrar Produto\n");
         printf("2 - Listar Produtos\n");
         printf("3 - Buscar Produto\n");
         printf("4 - Sair\n");
+        printf("Escolha uma opção: \n");
         scanf("%d", &choice);
         
         switch (choice) {
             case 1:
-                addProduct();
+				addProduct();
                 break;
             case 2:
-                listProducts();
+				listProducts();
                 break;
             case 3:
-                searchProduct();
+				searchProduct();
                 break;
             case 4:
-                printf("Saindo...\n");
+                printf("\nSaindo...\n");
                 break;
             default:
                 printf("Opção inválida! Tente novamente.\n");
@@ -57,10 +61,39 @@ void menu(){
 }
 
 int addProduct(){
-	printf("AddProduct");
+	if(cod >= MAX){
+		system("cls");
+		printf("Cadastro cheio");
+		system("cls");
+		return;
+	}
+	
+	Product item;
+	
+	system("cls");
+	fflush(stdin);
+	
+	printf("Informe o nome do produto: ");
+	  fgets(item.name, 50, stdin);
+	  item.name[strcspn(item.name, "\n")] = '\0';
+	  
+	//GERAR CÓGIGO ALEATÓRIO // CODE = "CÓDIGO GERADO ALEATÓRIAMENTE"
+	  
+	printf("Informe a data de validade do produto (DATA/MÊS/ANO): ");
+	  fgets(item.vality, 8, stdin);
+	  item.vality[strcspn(item.vality, "\n")] = '\0';
+	  
+	fflush(stdin);
+	
+	printf("Informe o preço do produto: ");
+	  scanf("%d", &item.price);
+	  
+	printf("Produto cadastrado com sucesso\n");
+	  
+	cod++;
+	
 	system("pause");
 	system("cls");
-	return 0;
 }
 
 int searchProduct(){
